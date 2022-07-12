2d43aded7b2753d7503fd678aa49caca9ce45ec4
gift56 <efeasiughu@gmail.com>
2022-07-12 13:02:52 +0100

    stying header menu

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import Header from '../Header/Header';
 import './hero.css';
 
 const Hero = () => {
     return (
         <div className='hero'>
             <div className="leftH">
-
+                <Header />
             </div>
             <div className="rightH">
-                
+                rightSide
             </div>
         </div>
     )
