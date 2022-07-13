df5eb336b0e1ac8e1a0705777179bcae35fb8053
gift56 <efeasiughu@gmail.com>
2022-07-13 13:29:35 +0100

    importing imgs

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './reasons.css';
+import image1 from '../../assets/image1.png'
+import image2 from '../../assets/image2.png'
+import image3 from '../../assets/image3.png'
+import image4 from '../../assets/image4.png'
+import nb from '../../assets/nb.png'
+import adidas from '../../assets/adidas.png'
+import nike from '../../assets/nike.png'
+import tick from '../../assets/tick.png'
 
 const Reasons = () => {
-  return (
-    <div>
-      Reasons
-    </div>
-  )
+    return (
+        <div className="Reasons" id='Reasons'>
+            <div className="leftR">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Reasons
