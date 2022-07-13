5d71600e474894d168436b848d52b435408dffe8
gift56 <efeasiughu@gmail.com>
2022-07-13 13:02:01 +0100

    started working on programs heading

---
+++ src/components/Programs/Programs.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './programs.css'
+
+const Programs = () => {
+    return (
+        <div className="programs" id="programs">
+            <div className="programsHeader">
+                <span className='stroke-text'>Explore our </span>
+                <span>Programs</span>
+                <span className='stroke-text'>to shape you</span>
+            </div>
+        </div>
+    )
+}
+
+export default Programs
