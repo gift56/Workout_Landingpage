129c61277fea87d6c597ce91ea1dc82f27422e5e
gift56 <efeasiughu@gmail.com>
2022-07-13 13:11:29 +0100

    done mapping the programs area

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './programs.css'
+import './programs.css';
+import { programsData } from '../../data/programsData';
+import RightArrow from '../../assets/rightArrow.png'
 
 const Programs = () => {
     return (
@@ -8,6 +10,19 @@ const Programs = () => {
                 <span className='stroke-text'>Explore our </span>
                 <span>Programs</span>
                 <span className='stroke-text'>to shape you</span>
+            </div>
+            <div className="programsCatergories">
+                {programsData.map((program) => (
+                    <div className="catergory">
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+                        <div className="joinNow">
+                            <span>Join Now</span>
+                            <img src={RightArrow} alt="" />
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
