69be59ec5c10f2c7bbe193604ca3b3dc56558930
gift56 <efeasiughu@gmail.com>
2022-07-14 15:13:46 +0100

    done with the animation part

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -3,8 +3,10 @@ import { testimonialsData } from '../../data/testimonialsData';
 import './testimonial.css';
 import leftArrow from '../../assets/leftArrow.png';
 import rightArrow from '../../assets/rightArrow.png';
+import { motion } from 'framer-motion';
 
 const Testimonial = () => {
+    const transition = { type: 'spring', duration: 3 }
     const [selected, setSelected] = useState(0);
     const tLength = testimonialsData.length;
     return (
@@ -24,9 +26,23 @@ const Testimonial = () => {
                 </span>
             </div>
             <div className="rightT">
-                <div></div>
-                <div></div>
-                <img src={testimonialsData[selected].image} alt="" />
+                <motion.div
+                    initial={{ opacity: 0, x: -100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ ...transition, duration: 2 }}
+                ></motion.div>
+                <motion.div
+                    initial={{ opacity: 0, x: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ ...transition, duration: 2 }}
+                ></motion.div>
+                <motion.img
+                    key={selected}
+                    initial={{ opacity: 0, x: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -100 }}
+                    transition={transition}
+                    src={testimonialsData[selected].image} alt="" />
                 <div className="arrows">
                     <img
                         onClick={() => selected === 0
