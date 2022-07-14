c1b71165b41b812cd587c6b8d84761740a8d2bb0
gift56 <efeasiughu@gmail.com>
2022-07-14 12:05:37 +0100

    working on testimonial area

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero';
 import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
+import Testimonial from './components/Testimonial/Testimonial';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Programs />
       <Reasons />
       <Plans />
+      <Testimonial />
     </div>
   );
 }
---
+++ src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './testimonial.css';
+
+const Testimonial = () => {
+    return (
+        <div className="Testimonials">
+            <div className="leftT">
+                <span>Testimonials</span>
+                <span className='stroke-text'>What they</span>
+                <span>say about us</span>
+            </div>
+            <div className="rightT"></div>
+        </div>
+    )
+}
+
+export default Testimonial
