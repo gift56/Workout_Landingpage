5f63fe3b1951da0f4afb9d8e0a21d3dd2c71f5e5
gift56 <efeasiughu@gmail.com>
2022-07-14 13:01:07 +0100

    working on email area

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './join.css';
+
+const Join = () => {
+    return (
+        <div className="Join" id="Join">
+            <div className="leftJ">
+                <hr />
+                <div>
+                    <span className='stroke-text'>READY TO</span>
+                    <span> LEVEL UP</span>
+                </div>
+                <div>
+                    <span>YOUR BODY</span>
+                    <span className='stroke-text'> WITH US?</span>
+                </div>
+            </div>
+            <div className="rightJ"></div>
+        </div>
+    )
+}
+
+export default Join
