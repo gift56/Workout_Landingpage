044a99d08a814a69ade87668103273858565c6d2
gift56 <efeasiughu@gmail.com>
2022-07-14 14:05:06 +0100

    done working on form area

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './join.css';
+import emailjs from '@emailjs/browser'
 
 const Join = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_ejgcmqp', 'template_we1hesf', form.current,
+         's-ou2ILPigjh0m_Xb')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
     return (
         <div className="Join" id="Join">
             <div className="leftJ">
@@ -12,10 +26,15 @@ const Join = () => {
                 </div>
                 <div>
                     <span>YOUR BODY</span>
-                    <span className='stroke-text'> WITH US?</span>
+                    <span className='stroke-text'>WITH US?</span>
                 </div>
             </div>
-            <div className="rightJ"></div>
+            <div className="rightJ">
+                <form ref={form} className="emailContainer" onSubmit={sendEmail}>
+                    <input type="email" name='user_email' placeholder='Enter your Email Address' />
+                    <button type='submit' className='btn btnJ'>Join Now</button>
+                </form>
+            </div>
         </div>
     )
 }
