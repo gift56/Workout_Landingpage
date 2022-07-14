2557f672e4e4fe4d1f034dec06f082027dfbac15
gift56 <efeasiughu@gmail.com>
2022-07-14 11:54:24 +0100

    done with the plans area

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { plansData } from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png'
 import './plans.css';
 
 const Plans = () => {
@@ -13,7 +14,21 @@ const Plans = () => {
             <div className="plans">
                 {plansData.map((plans, index) => (
                     <div className="plan" key={index}>
-
+                        {plans.icon}
+                        <span>{plans.name}</span>
+                        <span>$ {plans.price}</span>
+                        <div className="plansFeatures">
+                            {plans.features.map((feature, index) => (
+                                <div className="feature" key={index}>
+                                    <img src={whiteTick} alt="" />
+                                    <span>{feature}</span>
+                                </div>
+                            ))}
+                        </div>
+                        <div>
+                            <span>See more benefits -></span>
+                        </div>
+                        <button className='btn'>Join Now</button>
                     </div>
                 ))}
             </div>
