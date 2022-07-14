8d5e93a1ceb8772a2f8997e8fe4c528297fc6743
gift56 <efeasiughu@gmail.com>
2022-07-14 12:36:19 +0100

    styign img on testimonial area

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { testimonialsData } from '../../data/testimonialsData';
 import './testimonial.css';
+import leftArrow from '../../assets/leftArrow.png';
+import rightArrow from '../../assets/rightArrow.png';
 
 const Testimonial = () => {
+    const [selected, setSelected] = useState(0);
+    const tLength = testimonialsData.length;
     return (
         <div className="Testimonials">
             <div className="leftT">
                 <span>Testimonials</span>
                 <span className='stroke-text'>What they</span>
                 <span>say about us</span>
+                <span>
+                    {testimonialsData[selected].review}
+                </span>
+                <span>
+                    <span style={{ color: "var(--orange)" }}>
+                        {testimonialsData[selected].name}
+                    </span>{" "}
+                    - {testimonialsData[selected].status}
+                </span>
             </div>
-            <div className="rightT"></div>
+            <div className="rightT">
+                <div></div>
+                <div></div>
+                <img src={testimonialsData[selected].image} alt="" />
+                <div className="arrows">
+                    <img src={leftArrow} alt="" />
+                    <img src={rightArrow} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
