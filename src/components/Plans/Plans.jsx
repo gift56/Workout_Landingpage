d88dd0fa1fd13df4473e38c742c53223f1b0d5a7
gift56 <efeasiughu@gmail.com>
2022-07-14 11:15:14 +0100

    done with the plans header

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './plans.css';
+
+const Plans = () => {
+  return (
+    <div className="plansContainer">
+        <div className="programsHeader">
+            <span className='stroke-text'>READY TO START</span>
+            <span>YOUR JOURNEY</span>
+            <span className='stroke-text'>NOW WITH US</span>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Plans
